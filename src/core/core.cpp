4bc9ac6ce5d15e21b8763a726522b14a87b0ab58
sokolov ivan <sokolovivaned@protonmail.com>
2019-11-06 00:55:55 +0300

    dissolve cmds.cpp completely

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,8 +1,9 @@
-#include "inc_general.h"
 #include "core.h"
 #include "entity.h"
-#include "render/render.h"
+#include "inc_general.h"
+#include "input/cmds.h"
 #include "input/input.h"
+#include "render/render.h"
 
 namespace core
 {
@@ -102,4 +103,30 @@ void load_map (std::string path)
 	finalize();
 }
 
+} // namespace core
+
+COMMAND_ROUTINE (nop)
+{
+	return;
+}
+
+COMMAND_ROUTINE (exit)
+{
+	if (ev != PRESS)
+		return;
+	core::exit_code = 0;
+	if (!args.empty())
+		core::exit_code = std::atoi(args[0].c_str());
+	core::must_quit = true;
+}
+
+COMMAND_ROUTINE (echo)
+{
+	if (ev != PRESS)
+		return;
+
+	for (const std::string& arg: args)
+		std::cout << arg << " ";
+
+	std::cout << std::endl;
 }
